4d454cafc3b7d7e5107e9d2b6421c9858f393851
Hugo Fernandes <Hugoanf@hotmail.com>
2021-10-28 15:12:34 -0300

    Add project files.

---
+++ src/SGP.AplicationCore/Entity/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SGP.AplicationCore.Entity
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+    }
+}
---
+++ src/SGP.AplicationCore/Entity/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SGP.AplicationCore.Entity
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        [Display(Name = "Preço")]
+        public double Preco { get; set; }
+        public Categoria Categorias { get; set; }
+        public int CategoriaID { get; set; }
+    }
+}
