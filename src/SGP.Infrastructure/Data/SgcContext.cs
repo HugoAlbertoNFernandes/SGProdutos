d80d6a5f8de998084c117af8a6d1c09a25eb5727
Hugo Fernandes <Hugoanf@hotmail.com>
2021-10-28 16:27:41 -0300

    Cria context e migration

---
+++ src/SGP.Infrastructure/Data/SgcContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGP.AplicationCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGP.Infrastructure.Data
+{
+    public class SgcContext : DbContext
+    {
+        public SgcContext(DbContextOptions<SgcContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().ToTable("Produto");
+
+            modelBuilder.Entity<Categoria>().ToTable("Categoria");
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/SGP.Infrastructure/Migrations/20211028185752_inicial.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SGP.Infrastructure.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    CategoriaID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.CategoriaID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    ProdutoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Preco = table.Column<double>(nullable: false),
+                    CategoriaID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.ProdutoId);
+                    table.ForeignKey(
+                        name: "FK_Produto_Categoria_CategoriaID",
+                        column: x => x.CategoriaID,
+                        principalTable: "Categoria",
+                        principalColumn: "CategoriaID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_CategoriaID",
+                table: "Produto",
+                column: "CategoriaID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+        }
+    }
+}
